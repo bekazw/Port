692b79484b6649c33ae99719e4f71477f7879cf5
bekazw <bekazw@gmail.com>
2018-03-28 17:08:13 +0300

    redux 2

---
+++ port_0/src/actions/redux.js
@@ -0,0 +1,37 @@
+import {      
+    combineReducers,
+    createStore,
+  } from 'redux';
+//initial state
+  const initialState = {
+    currentSlide : 1
+  }  
+//actions
+  const a_slide = direction => {
+    return {
+      type: 'SLIDE',
+      direction
+    }
+  }
+//reducer
+  export const r_slide = (state = initialState, action) => {  
+    switch (action.type) {
+      case 'SLIDE':
+        return { currentSlide : action.SlideDirection};      
+      default:
+        return state;
+    }
+  };
+
+  ///
+  export const reducers = combineReducers({  
+    r_slide,
+  });
+  
+  // store.js
+  export function configureStore(initialState = {}) {  
+    const store = createStore(reducers, initialState);
+    return store;
+  };
+  
+  export const store = configureStore();  
